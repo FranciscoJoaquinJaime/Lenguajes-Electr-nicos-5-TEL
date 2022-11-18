1cfb82b51bfe68ae3f2a9aa86cb61338ea57459b
Pabloalmere <112029207+Pabloalmere@users.noreply.github.com>
2022-11-18 10:16:05 -0300

    Update Promedio.C

--- Promedio.C
+++ Promedio.C
@@ -1,28 +1,49 @@
 #include <stdio.h>
-#define NOTAS 20
-
+#define NOTAS 5
+int i=0;
+int suma=0;
+int aprobado=0;
+int desaprobado=0;
+float dividir;
+void ingreso(int vector[NOTAS]);
+void adicion (int vector[NOTAS]);
+void comparacion (int vector[NOTAS]);
+void promedio (int vector[NOTAS]);
 int vector [NOTAS];
 
 int main(void) {
-  int i=0;
-  int suma=0;
-  int aprobado=0;
-  int desaprobado=0;
-  float dividir;
   printf("Ingrese las calificaciones: \n");
+  ingreso (vector);
+  adicion (vector);
+  comparacion (vector);
+  promedio (vector);
+  return 0;
+}
+
+void ingreso(int vector[NOTAS]){
+  // int i=0;
   for(i=0;i<NOTAS;i++){
     scanf("%d", &vector[i]);
-  }
+   }
+}
+void adicion (int vector[NOTAS]){
+  //int suma=0;
   for(i=0;i<NOTAS;i++){
     suma=suma+vector[i];
-  }
+    }
+} 
+void comparacion (int vector[NOTAS]){
+  // int aprobado=0;
+  // int desaprobado=0;
   for(i=0;i<NOTAS;i++){
     if (vector[i]>=7) aprobado++;
     else desaprobado++;
   }
+}
+void promedio (int vector[NOTAS]){
+  // float dividir;
   dividir = ((float)suma/NOTAS);
   printf("El promedio es de: %.2f\n", dividir);
   printf("La cantidad de aprobados son: %d\n", aprobado);
   printf("Los alumnos que deben recuperar son: %d\n", desaprobado);
-  return 0;
 }
